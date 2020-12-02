e7edfe054950dcca31c3da497bdacd0cfb60ce68
Flavio Giobergia <flavio.giobergia@gmail.com>
2020-12-02 10:41:39 +0100

    we dont need no Counter

--- day02.py
+++ day02.py
@@ -1,5 +1,3 @@
-from collections import Counter
-
 if __name__ == "__main__":
     with open("day02.input") as f:
         lines = [ line.strip().split(" ") for line in f.readlines() ]
@@ -8,7 +6,7 @@ if __name__ == "__main__":
     count2 = 0 # counter for 2nd policy (XOR positions)
     for ab, letter, password in lines:
         a, b = map(int, ab.split("-"))
-        count1 += a <= Counter(password)[letter[0]] <= b
+        count1 += a <= password.count(letter[0]) <= b
         count2 += (password[a-1] == letter[0]) ^ (password[b-1] == letter[0])
     print(count1)
     print(count2)
